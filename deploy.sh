0fa699b1e6b1a834f67b73be5dbac35dee3a35e3
Jason <jasonnichols@C02R531HG8WM.local>
2016-10-23 22:24:49 +0900

    first pass at getting gce to work

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+echo $GCLOUD_SERVICE_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/jasonnic-hubot
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"jasonnic-hubot","image":"us.gcr.io/default-ecs-cluster/jasonnic-hubot:'"$CIRCLE_SHA1"'"}]}}}}'
+
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
