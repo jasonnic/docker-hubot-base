8f5d2c7ffd07fad026fab4c00a359b00145d8316
Jason <jasonnichols@C02R531HG8WM.local>
2016-10-26 10:12:26 +0900

    mods to deploy.sh to enable push to gcr

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,6 @@
 # Exit on any error
 set -e
 
-echo $GCLOUD_SERVICE_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
 sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/jasonnic-hubot
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"jasonnic-hubot","image":"us.gcr.io/default-ecs-cluster/jasonnic-hubot:'"$CIRCLE_SHA1"'"}]}}}}'
-
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
-sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-sudo /opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
+kubectl patch deployment jasonnic-hubot -p '{"spec":{"template":{"spec":{"containers":[{"name":"jasonnic-hubot","image":"us.gcr.io/default-ecs-cluster/jasonnic-hubot:'"$CIRCLE_SHA1"'"}]}}}}'
